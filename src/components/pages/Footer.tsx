035566c955befc882dfc6d65415b9c42a4193c06
Pranjal Rana <129268721+PRANJALRANA11@users.noreply.github.com>
2024-12-05 10:51:27 +0530

    chore: Update Footer component with new design and content

--- src/components/pages/Footer.tsx
+++ src/components/pages/Footer.tsx
@@ -2,41 +2,62 @@ import React from "react";
 import Link from "next/link";
 function Footer() {
   return (
-    <div>
-      <footer className=" rounded-lg shadow m-4 ">
-        <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-          <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
-            © 2023{" "}
-            <Link href="/" className="hover:underline">
-              ShopFusion™
-            </Link>
-            . All Rights Reserved.
-          </span>
-          <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
-            <li>
-              <Link href="#" className="hover:underline me-4 md:me-6">
-                About
-              </Link>
-            </li>
-            <li>
-              <Link href="#" className="hover:underline me-4 md:me-6">
-                Privacy Policy
-              </Link>
-            </li>
-            <li>
-              <Link href="#" className="hover:underline me-4 md:me-6">
-                Licensing
-              </Link>
-            </li>
-            <li>
-              <Link href="#" className="hover:underline">
-                Contact
-              </Link>
-            </li>
-          </ul>
+    <footer>
+      <div className="footer-container container">
+        <div className="content_1">
+          <img src="https://i.postimg.cc/htGyQ4JB/footer-logo.png" alt="logo" />
+          <p>
+            The customer is at the heart of our
+            <br />
+            unique business model, which includes
+            <br />
+            design.
+          </p>
+          <img src="https://i.postimg.cc/Nj9dgJ98/cards.png" alt="cards" />
         </div>
-      </footer>
-    </div>
+        <div className="content_2">
+          <h4>SHOPPING</h4>
+          <a href="#sellers">Clothing Store</a>
+          <a href="#sellers">Trending Shoes</a>
+          <a href="#sellers">Accessories</a>
+          <a href="#sellers">Sale</a>
+        </div>
+        <div className="content_3">
+          <h4>SHOPPING</h4>
+          <a href="./contact.html">Contact Us</a>
+          <a href="https://payment-method-sb.netlify.app/" target="_blank">
+            Payment Method
+          </a>
+          <a href="https://delivery-status-sb.netlify.app/" target="_blank">
+            Delivery
+          </a>
+          <a
+            href="https://codepen.io/sandeshbodake/full/Jexxrv"
+            target="_blank"
+          >
+            Return and Exchange
+          </a>
+        </div>
+        <div className="content_4">
+          <h4>NEWSLETTER</h4>
+          <p>
+            Be the first to know about new
+            <br />
+            arrivals, look books, sales & promos!
+          </p>
+          <div className="f-mail">
+            <input type="email" placeholder="Your Email" />
+            <i className="bx bx-envelope"></i>
+          </div>
+          <hr />
+        </div>
+      </div>
+      <div className="f-design">
+        <div className="f-design-txt container">
+          <p>Design and Code by code.Md Arman Ansari</p>
+        </div>
+      </div>
+    </footer>
   );
 }
 
